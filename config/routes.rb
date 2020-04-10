6dfe4d68de25ab020b1f05e40d4ba6892c5bf35c
SriHarshaSabbineni <sabbinenisriharsha8490@gmail.com>
2020-04-10 11:59:54 +0530

    posting

--- config/routes.rb
+++ config/routes.rb
@@ -2,24 +2,16 @@ Rails.application.routes.draw do
 
 
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  #Resource
-
-  #Todo is a resource
-
-  #-list of resources can be seen: index
-
-  #-single resource can be seen: show
-
-  #-create a new resource
+  #get "todos", to: "todos#index"
 
 
 
-  get "todos", to: "todos#index"
+  #post "todos", to: "todos#create"
 
 
 
-  get "todos/:id", to: "todos#show"
+  #get "todos/:id", to: "todos#show"
+
+  resources :todos
 
 end
